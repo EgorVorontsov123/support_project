b4ffb860fe6323e46b9c5c4543c84393c559767c
EgorVorontsov123 <voroncovegor60@gmail.com>
2024-12-19 00:13:32 +0300

    change wait-for-db

--- app/wait-for-db.sh
+++ app/wait-for-db.sh
@@ -14,7 +14,7 @@ done
 echo "Database is up - executing init script"
 
 # Выполняем SQL-скрипт
-psql -h "$host" -U "user" -d "postgres_db_av9c" -a -f /path/to/init.sql
+psql -h "$host" -U "user" -d "postgres_db_av9c" -a -f ./init.sql:/docker-entrypoint-initdb.d/init.sql
 
 echo "SQL script executed successfully"
 
