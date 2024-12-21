d3f666869a79ef801181f7eb94c38766cdfcc9c5
EgorVorontsov123 <voroncovegor60@gmail.com>
2024-12-21 16:13:06 +0300

    change script

--- app/wait-for-db.sh
+++ app/wait-for-db.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-host="$DATABASE_HOST"
+host="${DATABASE_HOST}"
 shift
 cmd="$@"
 
@@ -11,12 +11,5 @@ until netcat -z "$host" 5432; do
   sleep 2
 done
 
-echo "Database is up - executing init script"
-
-# Выполняем SQL-скрипт
-psql -h "$host" -U "user" -d "postgres_db_av9c" -a -f ./init.sql:/docker-entrypoint-initdb.d/init.sql
-
-echo "SQL script executed successfully"
-
-# Выполняем переданную команду
+echo "Database is up - executing command"
 exec $cmd
